63322b5d7c74c293c5e46a04fd16da258737789e
jabbott13153 <jabbott19@my.whitworth.edu>
2017-01-23 15:10:13 -0800

    Updating Project
    Created variables for Needles, Yarn, and Patterns, as well as get and set methods for them all. Also started on main GUI.

--- Final Project/Knitting App/Knitting App/src/Pattern.java
+++ Final Project/Knitting App/Knitting App/src/Pattern.java
@@ -1,4 +1,16 @@
+import java.awt.Image;
+
 
 public class Pattern {
-
+	private String addNotes = "";
+	private boolean circular = false;
+	private boolean doublePointed = false;
+	private boolean straight = false;
+	private boolean favorite = false;
+	private boolean inUse = false;
+	Image image;
+	int length = -1;
+	int size;
+	String brand = "";
+	String material = "";
 }
--- Final Project/Knitting App/Knitting App/src/Yarn.java
+++ Final Project/Knitting App/Knitting App/src/Yarn.java
@@ -1,16 +1,19 @@
 import java.awt.Image;
 
+
+//cloud address = cs1.whitworth.edu/jabbott19/name_of_file.jpg
 public class Yarn {
 	private String brand = "";
 	private int yardage = -1;
 	private String addNotes = "";
-	boolean favorite = false;
-	boolean inUse = false;
-	int lotNumber = -1;
-	Image image;
-	int weight;
-	String color;
-	String fiber;
+	private boolean favorite = false;
+	private boolean inUse = false;
+	private int howManySkeins = -1;
+	private int lotNumber = -1;
+	private Image image;
+	private int weight;
+	private String color;
+	private String fiber;
 	
 	Yarn(){}
 	Yarn(String c, String f, int w){
@@ -29,6 +32,7 @@ public class Yarn {
 	public void setColor(String c){color = c;}
 	public void setFiber(String f){fiber = f;}
 	public void setImage(Image i){image = i;}
+	public void setHowManySkeins(int hms){howManySkeins = hms;}
 	
 	public String getBrand(){return brand;}
 	public int getYardage(){return yardage;}
@@ -40,4 +44,5 @@ public class Yarn {
 	public int getWeight(){return weight;}
 	public String getColor(){return color;}
 	public String getFiber(){return fiber;}
+	public int getHowManySkeins(){return howManySkeins;}
 }
